3ac1ca359bd130f05b70b2bea676ae4a7deca298
Jayretch_Cooloosoo <39506395+RinzlerUchiha@users.noreply.github.com>
2026-06-10 17:04:29 +0800

    tentative

--- resources/views/pages/applicant/index.blade.php
+++ resources/views/pages/applicant/index.blade.php
@@ -10,10 +10,73 @@
     :root {
         --my-top-space: calc(var(--main-top-margin) + .25rem);
     }
+
+    #page-tabs {
+        width: 100%;
+        height: calc(100vh - var(--my-top-space));
+        max-height: calc(100vh - var(--my-top-space));
+        flex-wrap: nowrap;
+        /*transition: all 0.3s ease;*/
+        position: sticky;
+        top: var(--my-top-space);
+        border-right: 1px solid lightgray;
+        overflow: auto;
+    }
+
+    /* #page-tabs:hover {
+        overflow: auto;
+    } */
+
+    #page-tabs li a {
+        font-size: 12px;
+        color: black;
+    }
+
+    #page-tabs li a.active {
+        font-weight: bold;
+        color: var(--bs-primary);
+    }
+
+    #page-tabs li:hover {
+        background-color: #d1d1d1;
+    }
+
+    /* Adjusting scrollbar thickness */
+    #page-tabs::-webkit-scrollbar {
+        width: 7px;  /* Vertical scrollbar width */
+        height: 7px; /* Horizontal scrollbar height */
+    }
+
+    /* Customize the scrollbar thumb (draggable part) */
+    #page-tabs::-webkit-scrollbar-thumb {
+        background: #8b8a8a;  /* Color of the thumb */
+        border-radius: 10px;  /* Rounded corners for thumb */
+    }
+
+    .form-control-plaintext.border-bottom {
+        padding-bottom: 1px !important;
+    }
+
+    fieldset:disabled select {
+        -webkit-appearance: none; /* For Safari, Chrome */
+        -moz-appearance: none;    /* For Firefox */
+        appearance: none;         /* For modern browsers */
+        /*background: transparent;*/  /* Make the background transparent if needed */
+        border: none;             /* Optional: remove the border */
+    }
+
+    #page-tabs svg.bi {
+        height: 30px;
+        width: 30px;
+    }    
 </style>
 
 <script>
     $(function () {
+        const link_item = $("#page-tabs a[href='{{ config('app.url').'/applicant/list' }}']").parent()[0];
+        if(link_item){
+            link_item.scrollIntoView({ block: 'center' });
+        }
         let table = $('#applicant-table').DataTable({
             processing: true,
             serverSide: true,
@@ -63,7 +126,15 @@
     });
 </script>
 
-<div class="row pt-1 justify-content-center">
+<div class="row pt-1">
+    <div class="col-md-2">
+        <ul class="nav flex-column" id="page-tabs">
+            <li class="nav-item"><a href="{{ config('app.url') }}/applicant/list" class="nav-link gap-2 icon-link {{ $maincat == '' ? 'active' : '' }}">
+                <img class="rounded-circle" width="30" height="30" src="{{ asset('icon/applicant.png') }}"> Applicant List</a></li>
+            <li class="nav-item"><a href="{{ config('app.url') }}/applicant/list" class="nav-link gap-2 icon-link {{ $maincat == '' ? 'active' : '' }}">
+                <img class="rounded-circle" width="30" height="30" src="{{ asset('icon/applicant.png') }}"> Applicant List</a></li>
+        </ul>
+    </div>
     <div class="col-md-10">
         <h5>Applicant List</h5>
         <select id="status-filter" class="form-control form-control-sm">
